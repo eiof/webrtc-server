2d6a2f9776f75e7a8fb7d363c0338a184533565f
Peter Bertuglia <peter.bertuglia@dbtlabs.com>
2024-06-14 13:30:56 -0700

    Add Offer and Call and Collection to store calls

--- server/main.go
+++ server/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/google/uuid"
 )
 
 // callDocument:
@@ -16,6 +18,48 @@ type OfferResponse struct {
 	Id string `json:"id"`
 }
 
+type Offer struct {
+	Sdp  string `json:"sdp"`
+	Type string `json:"type"`
+}
+
+type IceCandidate struct {
+}
+
+type Call struct {
+	Offer            Offer
+	OfferCandidates  []*IceCandidate
+	AnswerCandidates []*IceCandidate
+}
+
+func (c *Call) String() string {
+	return fmt.Sprint("%+v | %+v | %+v", c.Offer, c.OfferCandidates, c.AnswerCandidates)
+}
+
+type CallCollection struct {
+	calls map[string]*Call
+}
+
+func NewCallCollection() *CallCollection {
+	return &CallCollection{
+		calls: make(map[string]*Call),
+	}
+}
+
+func (c *CallCollection) Call(id string) *Call {
+	// TODO: handle non-existence
+	call, _ := c.calls[id]
+	return call
+}
+
+func (c *CallCollection) AddCall(call *Call) string {
+	id := uuid.NewString()
+	c.calls[id] = call
+	return id
+}
+
+var callCollection = NewCallCollection()
+
 // Create an offer + call with a unique id
 // Server sent event for answers
 // Answer a call with the unique id
@@ -23,6 +67,16 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Post("/offer", func(w http.ResponseWriter, r *http.Request) {
+		var offer Offer
+		if err := json.NewDecoder(r.Body).Decode(&offer); err != nil {
+			fmt.Printf("Error unmarshaling request: %s\n", err)
+		}
+		fmt.Printf("Request offer: %+v", offer)
+		call := &Call{
+			Offer: offer,
+		}
+		call_id := callCollection.AddCall(call)
+		fmt.Printf("CallCollection (new_id: %s): %+v\n", call_id, callCollection)
 		o := &OfferResponse{Id: "test"}
 		res, _ := json.Marshal(o)
 		w.Write(res)
